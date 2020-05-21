59e43dfe9b5fcb00ab0645ace1c9cd2af243526d
Brett Rawlins <brawlins@deseretdigital.com>
2020-05-20 23:44:22 -0600

    Try using onInitialClientRender instead of onClientEntry for FOUC

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,7 +8,7 @@
 require("prismjs/themes/prism.css")
 
 // Used to prevent flash of unstyled content (FOUC). Remove this class after js loads.
-exports.onClientEntry = () => {
+exports.onInitialClientRender = () => {
   window.addEventListener("load", () => {
     document.body.className = document.body.className.replace(/\bno-js\b/, "")
   })
