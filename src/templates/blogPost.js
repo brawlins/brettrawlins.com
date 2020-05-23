74eea630272bb5aa5137542fd876dd9e2bfd77d7
Brett Rawlins <brawlins@deseretdigital.com>
2020-05-22 23:24:08 -0600

    Add tag links to blog post page

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -10,7 +10,7 @@ const Template = ({ data, pageContext }) => {
   const { next, prev } = pageContext
   const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
-  const { title, date } = frontmatter
+  const { title, date, tags } = frontmatter
 
   return (
     <Layout>
@@ -19,6 +19,12 @@ const Template = ({ data, pageContext }) => {
         <h1>{title}</h1>
         <p css={css`color: #aaa;`}>{formatDate(date)}</p>
         <div className="blogPost" dangerouslySetInnerHTML={{ __html: html }} />
+        <div css={tagsWrapper}>
+          {tags.length
+            ? tags.map(tagName => (<Link key={tagName} to={`/tags/${tagName}`}>#{tagName}</Link>))
+            : null
+          }
+        </div>
         {prev && <Link css={css`margin-right: 5px;`} to={getPathToPost(prev.frontmatter.title)}>&laquo; Prev</Link>}
         {next && <Link css={css`margin-left: 5px;`} to={getPathToPost(next.frontmatter.title)}>Next &raquo;</Link>}
       </div>
@@ -33,9 +39,19 @@ export const query = graphql`
       frontmatter {
         title
         date
+        tags
       }
     }
   }
 `
 
 export default Template
+
+const tagsWrapper = css`
+  display: flex;
+  margin-bottom: 1em;
+
+  a {
+    margin-right: 10px;
+  }
+`
