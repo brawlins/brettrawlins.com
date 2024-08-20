43afba068670b73d8c542a25f751f41e5e6eb1cf
Brett Rawlins <github.y2p7e@simplelogin.com>
2024-08-20 15:15:05 -0600

    small text change on About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ const About = () => {
     <Layout>
       <SEO title="About" />
       <h1>Hi! I'm Brett.</h1>
-      <p>I’m a web developer. I love building new things and learning about web technologies. This is where I write about things I learn. The more I learn, the more I realize how much I don’t know. This blog is primarily an archive of learning that I keep as a reference for myself, but hopefully it will prove useful to someone else too.</p>
+      <p>I’m a full-stack software engineer. I love building new things and learning about web technologies. This is where I write about things I learn. The more I learn, the more I realize how much I don’t know. This blog is primarily an archive of learning that I keep as a reference for myself, but hopefully it will prove useful to someone else too.</p>
     </Layout>
   )
 }
